66f0720007e047d1b7515c3243969f5f455dc853
kronosGR <geo.elgeo@gmail.com>
2022-05-17 07:09:09 +0200

    added find form

---
+++ SimpleNotepad/Form2.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SimpleNotepad
+{
+    partial class frmFind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFindWhat = new System.Windows.Forms.TextBox();
+            this.chkMatchCase = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoUp = new System.Windows.Forms.RadioButton();
+            this.rdoDown = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(338, 25);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find Next";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(338, 54);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Find what:";
+            // 
+            // textFindWhat
+            // 
+            this.textFindWhat.Location = new System.Drawing.Point(95, 22);
+            this.textFindWhat.Name = "textFindWhat";
+            this.textFindWhat.Size = new System.Drawing.Size(100, 20);
+            this.textFindWhat.TabIndex = 3;
+            // 
+            // chkMatchCase
+            // 
+            this.chkMatchCase.AutoSize = true;
+            this.chkMatchCase.Location = new System.Drawing.Point(56, 58);
+            this.chkMatchCase.Name = "chkMatchCase";
+            this.chkMatchCase.Size = new System.Drawing.Size(83, 17);
+            this.chkMatchCase.TabIndex = 4;
+            this.chkMatchCase.Text = "Match Case";
+            this.chkMatchCase.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoDown);
+            this.groupBox1.Controls.Add(this.rdoUp);
+            this.groupBox1.Location = new System.Drawing.Point(56, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 69);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Direction";
+            // 
+            // rdoUp
+            // 
+            this.rdoUp.AutoSize = true;
+            this.rdoUp.Location = new System.Drawing.Point(18, 32);
+            this.rdoUp.Name = "rdoUp";
+            this.rdoUp.Size = new System.Drawing.Size(39, 17);
+            this.rdoUp.TabIndex = 0;
+            this.rdoUp.TabStop = true;
+            this.rdoUp.Text = "Up";
+            this.rdoUp.UseVisualStyleBackColor = true;
+            // 
+            // rdoDown
+            // 
+            this.rdoDown.AutoSize = true;
+            this.rdoDown.Location = new System.Drawing.Point(84, 32);
+            this.rdoDown.Name = "rdoDown";
+            this.rdoDown.Size = new System.Drawing.Size(53, 17);
+            this.rdoDown.TabIndex = 1;
+            this.rdoDown.TabStop = true;
+            this.rdoDown.Text = "Down";
+            this.rdoDown.UseVisualStyleBackColor = true;
+            // 
+            // frmFind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 232);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chkMatchCase);
+            this.Controls.Add(this.textFindWhat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFind);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFind";
+            this.Text = "Find";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFindWhat;
+        private System.Windows.Forms.CheckBox chkMatchCase;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoUp;
+        private System.Windows.Forms.RadioButton rdoDown;
+    }
+}
